706d1583a34d7d908dbbb2f6b635d7515efacc7c
DESKTOP-OIO0DRA\nnnyy <nnnyyy@nate.com>
2017-08-31 06:42:47 +0900

    애니메이션 연습

--- ddorang2017_android/DDoRang/app/src/main/java/kaltznnnyyy/com/naver/blog/nexonddorang/MainActivity.java
+++ ddorang2017_android/DDoRang/app/src/main/java/kaltznnnyyy/com/naver/blog/nexonddorang/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,18 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ConstraintLayout layout = (ConstraintLayout)findViewById(R.id.content_main);
-        View v = new View(getApplicationContext());
+        final View v = new View(getApplicationContext());
 
         Point pt = new Point();
         getWindowManager().getDefaultDisplay().getSize(pt);
         v.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100));
         v.setBackgroundColor(Color.RED);
-        v.setX(10);
-        v.setY(100);
         layout.addView(v);
+        v.setVisibility(View.INVISIBLE);
 
+        final Animation animSizeUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.sizeup);
         TextView tv = (TextView)findViewById(R.id.tv_test);
         tv.setText(pt.toString());
+        tv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View _v, MotionEvent event) {
+                if(!animSizeUp.hasStarted() || animSizeUp.hasEnded()) {
+                    v.animate().y(_v.getY());
+                    v.setVisibility(View.VISIBLE);
+                    v.startAnimation(animSizeUp);
+                }
+                return false;
+            }
+        });
+
+        Animation animBlink = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        tv.startAnimation(animBlink);
     }
 
     @Override
